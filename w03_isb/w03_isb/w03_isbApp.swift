61214355a75dc87940fb8a3b9dac3af23699370f
Celin <127372089+celinka12@users.noreply.github.com>
2024-11-01 09:05:56 +0700

    Add files via upload

---
+++ w03_isb/w03_isb/w03_isbApp.swift
@@ -0,0 +1,17 @@
+//
+//  w03_isbApp.swift
+//  w03_isb
+//
+//  Created by student on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct w03_isbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
