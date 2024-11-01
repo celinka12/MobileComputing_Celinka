66f1c1b2be9066e9362b740a3259c03b77b10c08
Celin <127372089+celinka12@users.noreply.github.com>
2024-11-01 09:06:27 +0700

    Add files via upload

---
+++ w04_isb/w04_isb/w04_isbApp.swift
@@ -0,0 +1,17 @@
+//
+//  w04_isbApp.swift
+//  w04_isb
+//
+//  Created by student on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct w04_isbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ w04_isb/w04_isbTests/w04_isbTests.swift
@@ -0,0 +1,17 @@
+//
+//  w04_isbTests.swift
+//  w04_isbTests
+//
+//  Created by student on 11/10/24.
+//
+
+import Testing
+@testable import w04_isb
+
+struct w04_isbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
